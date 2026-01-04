c70411a3cc60e0fd19037f668e376980c231a905
valeriy <vlrp512@gmail.com>
2026-01-04 18:26:14 +0500

    Shared: add new resources

--- shared/src/commonMain/kotlin/app/xl/sportappkmp/models/Equipment.kt
+++ shared/src/commonMain/kotlin/app/xl/sportappkmp/models/Equipment.kt
@@ -3,23 +3,24 @@ package app.xl.sportappkmp.models
 import app.xl.sportappkmp.MR
 import app.xl.sportappkmp.interfaces.LocalizedEnum
 import app.xl.sportappkmp.utils.Localizer
+import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.StringResource
 
-enum class Equipment(val titleRes: StringResource): LocalizedEnum {
-    PLATE(MR.strings.plate),
-    MACHINE(MR.strings.machine),
-    CARDIO_MACHINE(MR.strings.cardioMachine),
-    KETTLEBELL(MR.strings.kettlebell),
-    DUMBBELL(MR.strings.dumbbell),
-    BARBELL(MR.strings.barbell),
-    RESISTANCE_BAND(MR.strings.resistanceBand),
-    BALL(MR.strings.ball),
-    BENCH(MR.strings.bench),
-    PARALLEL_BARS(MR.strings.parallelBars),
-    PULL_UP_BAR(MR.strings.pullUpBar),
-    ROPE(MR.strings.rope),
-    BODY_WEIGHT(MR.strings.bodyweight),
-    OTHER(MR.strings.other);
+enum class Equipment(val titleRes: StringResource, val imageRes: ImageResource): LocalizedEnum {
+    PLATE(MR.strings.plate, MR.images.eqPlate),
+    MACHINE(MR.strings.machine, MR.images.eqMachine),
+    CARDIO_MACHINE(MR.strings.cardioMachine, MR.images.eqCardioMachine),
+    KETTLEBELL(MR.strings.kettlebell, MR.images.eqKettlebell),
+    DUMBBELL(MR.strings.dumbbell, MR.images.eqDumbbell),
+    BARBELL(MR.strings.barbell, MR.images.eqBarbell),
+    RESISTANCE_BAND(MR.strings.resistanceBand, MR.images.eqResistanceBand),
+    BALL(MR.strings.ball, MR.images.eqBall),
+    BENCH(MR.strings.bench, MR.images.eqBench),
+    PARALLEL_BARS(MR.strings.parallelBars, MR.images.eqParallelBars),
+    PULL_UP_BAR(MR.strings.pullUpBar, MR.images.eqParallelBars),
+    ROPE(MR.strings.rope, MR.images.eqRope),
+    BODY_WEIGHT(MR.strings.bodyweight, MR.images.eqBodyweight),
+    OTHER(MR.strings.other, MR.images.eqOther);
 
     override fun localizedTitle(
         localizer: Localizer
--- shared/src/commonMain/kotlin/app/xl/sportappkmp/models/MuscleGroup.kt
+++ shared/src/commonMain/kotlin/app/xl/sportappkmp/models/MuscleGroup.kt
@@ -3,18 +3,22 @@ package app.xl.sportappkmp.models
 import app.xl.sportappkmp.MR
 import app.xl.sportappkmp.interfaces.LocalizedEnum
 import app.xl.sportappkmp.utils.Localizer
+import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.StringResource
 
-enum class MuscleGroup(val titleRes: StringResource): LocalizedEnum {
-    CHEST(MR.strings.chest),
-    BACK(MR.strings.back),
-    LEGS(MR.strings.legs),
-    ARMS(MR.strings.arms),
-    SHOULDERS(MR.strings.shoulders),
-    CORE_ABS(MR.strings.coreAbs),
-    NECK(MR.strings.neck),
-    CARDIO(MR.strings.cardio),
-    OTHER(MR.strings.other);
+enum class MuscleGroup(
+    val titleRes: StringResource,
+    val imageRes: ImageResource
+): LocalizedEnum {
+    CHEST(MR.strings.chest, MR.images.mgChest),
+    BACK(MR.strings.back, MR.images.mgBack),
+    LEGS(MR.strings.legs, MR.images.mgLegs),
+    ARMS(MR.strings.arms, MR.images.mgArm),
+    SHOULDERS(MR.strings.shoulders, MR.images.mgShoulders),
+    CORE_ABS(MR.strings.coreAbs, MR.images.mgAbs),
+    NECK(MR.strings.neck, MR.images.mgNeck),
+    CARDIO(MR.strings.cardio, MR.images.mgCardio),
+    OTHER(MR.strings.other, MR.images.mgOther);
 
     override fun localizedTitle(
         localizer: Localizer
