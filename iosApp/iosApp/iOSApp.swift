923c3f6a4b95537af01bd07fb5ab3b070693bd84
valeriy <vlrp512@gmail.com>
2026-01-21 22:27:46 +0500

    iOS: check first launch

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,26 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        checkFirstLaunch()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainScreen()
+        }
+    }
+    
+    func checkFirstLaunch() {
+        let factory = KmpSettingsFactory()
+        let settings = factory.isFirstLaunch()
+        let manager = FirstLaunchManager(settings: settings)
+        
+        if manager.checkFirstLaunch() {
+            DemoWorkouts.shared.createDemoWorkouts(fileManager: Shared.FileManager())
         }
     }
 }
