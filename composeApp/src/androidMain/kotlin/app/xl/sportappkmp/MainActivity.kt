ab95df5f81522f59d6b4f293ab33487caa08a3f2
valeriy <vlrp512@gmail.com>
2026-01-21 22:28:51 +0500

    Android: check first launch

--- composeApp/src/androidMain/kotlin/app/xl/sportappkmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/app/xl/sportappkmp/MainActivity.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import app.xl.sportappkmp.data.DemoWorkouts
+import app.xl.sportappkmp.data.FileManager
+import app.xl.sportappkmp.utils.FirstLaunchManager
+import app.xl.sportappkmp.utils.KmpSettingsFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        checkFirstLaunch()
+
         setContent {
             MainScreen()
+        }
+    }
+
+    private fun checkFirstLaunch() {
+        val factory = KmpSettingsFactory(applicationContext)
+        val manager = FirstLaunchManager(factory.isFirstLaunch())
+
+        val isFirst = manager.checkFirstLaunch()
+        if (isFirst) {
+            DemoWorkouts.createDemoWorkouts(FileManager(context = applicationContext))
         }
     }
 }
